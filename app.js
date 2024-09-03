adf67601cb272bca2486e9b6a34778e58727dcb8
Anthony-Faria-dos-santos <anthony.faria-dos-santos@oclock.school>
2024-09-03 18:18:31 +0200

    feat(app.js): add dotenv configuration to manage environment variables fix(app.js): update server port to use environment variable or default value feat(app.js): improve response message formatting in the root route feat(package.json): add nodemon as a dependency for automatic server restarts feat(package.json): add start script to run the application with nodemon

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const express    = require('express');
+require('dotenv').config();
+const express = require('express');
 const app = express();
 
-app.get('/', function(req,res) {
-    res.send('<b>Hello My World<b>!');
+let PORT = process.env.PORT || 4000;
+
+app.get('/', function(req, res) {
+    res.send('<b>Hello My World!</b>');
 });
 
-app.listen(3000,function () {
-    console.log('Server started on port 3000');
-})
+app.listen(PORT, function () {
+    console.log(`Server started on port ${PORT}`);
+});
