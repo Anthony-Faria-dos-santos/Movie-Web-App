1dfa358b9b660e68f0254b343838b04ec28e074d
Anthony-Faria-dos-santos <anthony.faria-dos-santos@oclock.school>
2024-09-11 18:28:18 +0200

    ✨ (app.js): add a new /movies route to enhance the API functionality ♻️ (.gitignore): update .gitignore to include .env.bk for better file management 📝 (package.json): change license from ISC to MIT to align with project goals 📝 (settings.json): add initial VSCode settings file for project configuration 🐛 (.DS_Store): remove .DS_Store from version control to avoid clutter

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-let PORT = process.env.PORT || 4000;
+let PORT = process.env.PORT || 4000; 
 
 app.get('/', function(req, res) {
     res.send('<b>Hello My World!</b>');
 });
 
+app.get('/movies', function(req, res) {
+    res.send('<b>Hello My World!</b>');
+}) 
+
 app.listen(PORT, function () {
     console.log(`Server started on port ${PORT} 🙂 `);
 });
+
